76eca69b345ed985aa7ec29e0644b6deacabc538
luismiguelsaez <luismiguelsaez83@gmail.com>
2020-12-20 11:09:56 +0100

    Added database test endpoint

--- app/code/app.py
+++ app/code/app.py
@@ -1,7 +1,25 @@
 from flask import Flask
+from os import environ
+import pymongo
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db/test')
+def db_test():
+    try:
+        mongoHost = environ['MONGO_HOST']
+        mongoPort = environ['MONGO_PORT']
+        mongoDb = environ['MONGO_DB']
+    except Exception as excEnv:
+        return {"result":"ERROR","message":"Failure while retrieving environment variables"}, 500
+
+    try:
+        con = pymongo.MongoClient(mongoHost,int(mongoPort))
+    except Exception as excDb:
+        return {"result":"ERROR","message":"Failure while creating database connection"}, 500
+
+    return {"result":"OK"}
--- test/code/main.py
+++ test/code/main.py
@@ -4,3 +4,7 @@ from os import environ
 def test_get_base_status_code_equals_200():
      response = get(environ.get('ENDPOINT'))
      assert response.status_code == 200
+
+def test_get_db_test_equals_200():
+     response = get(environ.get('ENDPOINT') + "/db/test")
+     assert response.status_code == 200
